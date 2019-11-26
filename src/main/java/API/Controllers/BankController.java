4c0ff7507bcedeb7060491d3864665d1b1fd1ac2
stein <info@steinmilder.nl>
2019-11-26 15:25:39 +0100

    Koppelfunctie toegevoegd

---
+++ src/main/java/API/Controllers/BankController.java
@@ -0,0 +1,46 @@
+package API.Controllers;
+
+import API.Adapter.Adapter;
+import API.Adapter.BankAdapter;
+import API.DTO.Bank;
+import API.DTO.BankToken;
+import API.Services.UserService;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+
+@Path("/connect")
+public class BankController {
+    private static final String BANK_TOKEN = "{{BANK}}";
+    private static final String REDIRECT_URI = "http://localhost:8080/connect/"+BANK_TOKEN+"/finish";
+    private static final URI FINAL_REDIRECT_URL = URI.create("http://localhost:4200/overzicht/rekeningen");
+
+    private UserService userService;
+
+    @Inject
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Path("/{bank}")
+    @GET
+    public Response connect(@PathParam("bank") Bank bank, @QueryParam("token") String token) {
+        Adapter adapter = new BankAdapter(bank);
+        var redirectUrl = REDIRECT_URI.replace(BANK_TOKEN, bank.toString());
+        var url = adapter.getAuthorizationUrl(redirectUrl, token);
+        return Response.temporaryRedirect(url).build();
+    }
+
+    @Path("/{bank}/finish")
+    @Produces(MediaType.APPLICATION_JSON)
+    @GET
+    public Response finish(@PathParam("bank") Bank bank, @QueryParam("code") String code, @QueryParam("state") String token) {
+        Adapter adapter = new BankAdapter(bank);
+        BankToken bankToken = adapter.token(code);
+        bankToken.setBank(bank);
+        userService.attachBankAccount(token, bankToken);
+        return Response.temporaryRedirect(FINAL_REDIRECT_URL).build();
+    }
+}
---
+++ src/main/java/API/Controllers/DummyController.java
@@ -0,0 +1,28 @@
+package API.Controllers;
+
+import API.DTO.Bank;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Path("/dummy")
+public class DummyController {
+    private static final String DEFAULT_ING_AUTHORIZATION_CODE = "2c1c404c-c960-49aa-8777-19c805713edf";
+
+    @Path("/ing")
+    @GET
+    public Response authorizeIng(@QueryParam("redirect_uri") String uri, @QueryParam("state") String state) {
+        try {
+            URI url = new URI(uri+"?code="+DEFAULT_ING_AUTHORIZATION_CODE+"&state="+state);
+            return Response.temporaryRedirect(url).build();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
