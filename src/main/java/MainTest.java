7f44da050b858ec5eb1ad6af763d5f55e28e58f9
stein <info@steinmilder.nl>
2019-11-26 11:06:36 +0100

    ING refresh token mock

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -1,10 +1,13 @@
+import API.DTO.BankToken;
+import API.ING.Service.INGAccountService;
+import API.ING.Util.INGUtil;
 import API.RABO.Service.RabobankService;
+import com.google.gson.Gson;
 
 public class MainTest {
-
     public static void main(String[] args) {
-        RabobankService service = new RabobankService();
-
-        service.refresh("AAIo1-2rR7L-Yr1U8A54uvvTlslbuFuX1f_aEqAMkHy1D6FGbP7sYWatwBnp0g29iKm3arKP76cBghxnsJ2GpaBWGNByKKCJFlkoxnwihjcb_LeAbqzdYUi0dYSeT3bj9Oe8f9M4L5h5x0TC0V9zw-aXc5jKKcmri4ZBCVBACMnPLow-ZWe7DztZKO-SvoFRMNT_ARHXAZ4aHOXD8kLnreyAe69vALaRsN3-cDSSjIuugVuqGmNXqracvYYUHLNCZuk");
+        var service = new INGAccountService();
+        var test = service.refresh("ff");
+        System.out.println(test.getAccessToken());
     }
 }
