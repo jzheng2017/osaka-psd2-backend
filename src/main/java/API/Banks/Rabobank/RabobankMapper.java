96f69b033261577ca48c32700340da316d63c265
Tristan Boland <T.Boland1@student.han.nl>
2019-12-10 11:53:03 +0100

    SonarQube Fixes

--- src/main/java/API/Banks/Rabobank/RabobankMapper.java
+++ src/main/java/API/Banks/Rabobank/RabobankMapper.java
@@ -19,53 +19,42 @@ public class RabobankMapper {
     public AccountDetails mapToAccountDetails(JsonObject response) {
         var account = gson.fromJson(response.getAsJsonObject("account").toString(), Account.class);
         var bookedTransactions = response.getAsJsonObject("transactions").getAsJsonArray("booked");
-
         var transactions = new ArrayList<Transaction>();
-
         for(JsonElement element : bookedTransactions) {
             var object = element.getAsJsonObject();
-
-            var transaction = new Transaction();
-            transaction.setId(object.get("entryReference").getAsString());
-            transaction.setDate(object.get("bookingDate").getAsString());
-
-            var receiver = object.get("creditorAccount").getAsJsonObject();
-            var receiverAccount = new Account();
-            receiverAccount.setType(null);
-            receiverAccount.setIban(receiver.get("iban").getAsString());
-            if(object.has("creditorName")) {
-                receiverAccount.setName(object.get("creditorName").getAsString());
-            }
-            transaction.setReceiver(receiverAccount);
-
-            var sender = object.get("debtorAccount").getAsJsonObject();
-            var senderAccount = new Account();
-            senderAccount.setType(null);
-            senderAccount.setIban(sender.get("iban").getAsString());
-
-            if(object.has("debtorName")) {
-                senderAccount.setName(object.get("debtorName").getAsString());
-            }
-
-            transaction.setSender(senderAccount);
-
-            var transactionAmount = object.get("transactionAmount").getAsJsonObject();
-            var amount = transactionAmount.get("amount").getAsString();
-            transaction.setAmount(amount);
-
-            transaction.setType("Doorgifte");
-
-            transaction.setBooked(true);
-            transaction.setReceived(receiverAccount.getIban().equals(account.getIban()));
-
-            transactions.add(transaction);
+            transactions.add(getTransaction(account, object));
         }
-
-
         var details = new AccountDetails();
         details.setTransactions(transactions);
         details.setAccount(account);
-
         return details;
+    }
+
+    private Transaction getTransaction(Account account, JsonObject object) {
+        var transaction = new Transaction();
+        transaction.setId(object.get("entryReference").getAsString());
+        transaction.setDate(object.get("bookingDate").getAsString());
+        var receiver = object.get("creditorAccount").getAsJsonObject();
+        Account receiverAccount = getAccount(object, receiver, "creditorName");
+        transaction.setReceiver(receiverAccount);
+        var sender = object.get("debtorAccount").getAsJsonObject();
+        transaction.setSender(getAccount(object, sender, "debtorName"));
+        var transactionAmount = object.get("transactionAmount").getAsJsonObject();
+        var amount = transactionAmount.get("amount").getAsString();
+        transaction.setAmount(amount);
+        transaction.setType("Doorgifte");
+        transaction.setBooked(true);
+        transaction.setReceived(receiverAccount.getIban().equals(account.getIban()));
+        return transaction;
+    }
+
+    private Account getAccount(JsonObject object, JsonObject receiver, String creditorName) {
+        var receiverAccount = new Account();
+        receiverAccount.setType(null);
+        receiverAccount.setIban(receiver.get("iban").getAsString());
+        if (object.has(creditorName)) {
+            receiverAccount.setName(object.get(creditorName).getAsString());
+        }
+        return receiverAccount;
     }
 }
--- src/main/java/API/Errors/Error.java
+++ src/main/java/API/Errors/Error.java
@@ -1,6 +1,9 @@
 package API.Errors;
 
 public class Error {
+    private Error() {
+
+    }
     public static final String INVALID_TOKEN = "INVALID_TOKEN";
     public static final String INVALID_TABLEID = "INVALID_TABLEID";
     public static final String INVALID_ID = "INVALID_ID";
