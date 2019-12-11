69961532d9d2fd1f4e487a7871ba92a6623db30b
Tristan Boland <T.Boland1@student.han.nl>
2019-12-11 10:52:27 +0100

    Vulnerabilities daadwerkelijk opgelost

--- src/main/java/API/Banks/Requests/Headers.java
+++ src/main/java/API/Banks/Requests/Headers.java
@@ -1,6 +1,17 @@
 package API.Banks.Requests;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class Headers {
+    private static final Properties properties = new Properties();
+    static {
+        try {
+            properties.load(Headers.class.getClassLoader().getResourceAsStream("ip.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String DIGEST = "Digest";
     public static final String DATE = "Date";
@@ -12,6 +23,6 @@ public class Headers {
     public static final String TPP_REDIRECT_URI = "TPP-Redirect-URI";
     public static final String PSU_IP_ADDRESS = "PSU-IP-Address";
     public static final String X_IBM_CLIENT_ID = "x-ibm-client-id";
-    public static final String HARDCODED_IP_PAYMENT_REQUEST = "156.114.161.8";
-    public static final String HARDCODED_IP_RABOBANK = "99.154.223.227";
+    public static final String HARDCODED_IP_PAYMENT_REQUEST = properties.getProperty("paymentrequest.ip");
+    public static final String HARDCODED_IP_RABOBANK = properties.getProperty("rabobank.ip");
 }
