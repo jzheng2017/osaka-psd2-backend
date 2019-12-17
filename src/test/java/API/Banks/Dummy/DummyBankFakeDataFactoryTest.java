744515efa4a9fff6de6c2a4e21c4cd9e4afcad0b
Jiankai Zheng <J.Zheng1@student.han.nl>
2019-12-17 11:28:52 +0100

    add: unit tests for factory

---
+++ src/test/java/API/Banks/Dummy/DummyBankFakeDataFactoryTest.java
@@ -0,0 +1,36 @@
+package API.Banks.Dummy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+public class DummyBankFakeDataFactoryTest {
+    private DummyBankFakeDataFactory sut;
+
+    @BeforeEach
+    void setup() {
+        sut = spy(new DummyBankFakeDataFactory());
+    }
+
+    @Test
+    void getAccountsReturnsListOfAccounts(){
+        Assertions.assertNotNull(sut.getAccounts());
+    }
+    @Test
+    void getAccountsReturnsListOfAccountsNotEmpty(){
+        Assertions.assertTrue(sut.getAccounts().size() > 0);
+    }
+
+    @Test
+    void getBalanceReturnsBalance(){
+        Assertions.assertNotNull(sut.getBalanceFromAccounts("1"));
+    }
+
+    @Test
+    void getBalanceCallsGetAccountsFunction(){
+        sut.getBalanceFromAccounts("1");
+        verify(sut).getAccounts();
+    }
+}
