7e4a2f1ad7fe97528440298aa279d93992a0f42f
stein <stein@slimwapp.com>
2020-01-13 21:37:19 +0100

    Fixed some unit tests

--- src/main/java/API/Controllers/DummyController.java
+++ src/main/java/API/Controllers/DummyController.java
@@ -8,20 +8,12 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import java.net.URI;
-import java.util.logging.Logger;
 
 @Path("/dummy")
 public class DummyController {
     private static final String DEFAULT_ING_AUTHORIZATION_CODE = "2c1c404c-c960-49aa-8777-19c805713edf";
     private static final String DEFAULT_DUMMY_AUTHORIZATION_CODE = "dummy";
-    private static final Logger LOGGER = Logger.getLogger(DummyController.class.getName());
 
-    /**
-     * @param bank
-     * @param uri
-     * @param state
-     * @return
-     */
     @Path("/{bank}")
     @GET
     public Response authorize(@PathParam("bank") String bank, @QueryParam("redirect_uri") String uri, @QueryParam("state") String state) {
