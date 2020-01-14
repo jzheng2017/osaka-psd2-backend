e7ab5f61aa238b167322b7574a7c0ea7437c1b82
stein <stein@slimwapp.com>
2020-01-14 09:26:00 +0100

    Unit test voor DummyController en DummyService toegevoegd

--- src/main/java/API/Controllers/DummyController.java
+++ src/main/java/API/Controllers/DummyController.java
@@ -1,24 +1,28 @@
 package API.Controllers;
 
 import API.DTO.Bank;
-
+import API.Services.DummyService;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
-import java.net.URI;
 
 @Path("/dummy")
 public class DummyController {
-    private static final String DEFAULT_ING_AUTHORIZATION_CODE = "2c1c404c-c960-49aa-8777-19c805713edf";
-    private static final String DEFAULT_DUMMY_AUTHORIZATION_CODE = "dummy";
+    private DummyService dummyService;
+
+    @Inject
+    public void setDummyService(DummyService dummyService) {
+        this.dummyService = dummyService;
+    }
 
     @Path("/{bank}")
     @GET
-    public Response authorize(@PathParam("bank") String bank, @QueryParam("redirect_uri") String uri, @QueryParam("state") String state) {
-        var code = Bank.valueOf(bank).equals(Bank.ING) ? DEFAULT_ING_AUTHORIZATION_CODE : DEFAULT_DUMMY_AUTHORIZATION_CODE;
-        var url = URI.create(uri+"?code="+code+"&state="+state);
-        return Response.temporaryRedirect(url).build();
+    public Response authorize(@PathParam("bank") Bank bank, @QueryParam("redirect_uri") String uri, @QueryParam("state") String state) {
+        return Response
+                .temporaryRedirect(dummyService.authorize(bank, uri, state))
+                .build();
     }
 }
--- src/test/java/API/Controllers/DummyControllerTest.java
+++ src/test/java/API/Controllers/DummyControllerTest.java
@@ -1,11 +1,17 @@
 package API.Controllers;
 
+import API.DTO.Bank;
+import API.Services.DummyService;
+import API.Services.DummyServiceTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import javax.ws.rs.core.Response;
 import java.net.URI;
-import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,35 +19,33 @@ class DummyControllerTest {
     private static final String DEFAULT_ING_AUTHORIZATION_CODE = "2c1c404c-c960-49aa-8777-19c805713edf";
 
     private DummyController dummyController;
+    private DummyService mockedDummyService;
+    private Response expected;
 
     @BeforeEach
     void setup() {
         dummyController = new DummyController();
+        mockedDummyService = Mockito.mock(DummyService.class);
+        dummyController.setDummyService(mockedDummyService);
+
+        var url = URI.create("URL");
+        Mockito.when(mockedDummyService.authorize(Mockito.any(), Mockito.anyString(), Mockito.anyString())).thenReturn(url);
+        expected = Response.temporaryRedirect(url).build();
     }
 
-    @Test
-    void testAuthorizeIng() throws URISyntaxException {
+    void testAuthorizeForBank(Bank bank) {
         // Arrange
-        var url = "uri?code=" + DEFAULT_ING_AUTHORIZATION_CODE + "&state=state";
-        var expected = Response.temporaryRedirect(new URI(url)).build();
 
         // Act
-        var result = dummyController.authorize("ING","uri", "state");
+        var result = dummyController.authorize(bank,"uri", "state");
 
         // Assert
         assertEquals(expected.getStatus(), result.getStatus());
     }
 
     @Test
-    void testAuthorizeDummy() throws URISyntaxException {
-        // Arrange
-        var url = "uri?code=" + DEFAULT_ING_AUTHORIZATION_CODE + "&state=state";
-        var expected = Response.temporaryRedirect(new URI(url)).build();
-
-        // Act
-        var result = dummyController.authorize("DUMMY","uri", "state");
-
-        // Assert
-        assertEquals(expected.getStatus(), result.getStatus());
+    void testAuthorizeForBanks() {
+        List<Bank> banks = new ArrayList<>(Arrays.asList(Bank.DUMMY, Bank.ING));
+        banks.forEach(this::testAuthorizeForBank);
     }
 }
